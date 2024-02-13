f4c8dede00c4efea97969245d977af741c8c6057
Nabeel Yaseen <nabeelyaseenggwp@gmail.com>
2024-02-14 03:30:04 +0500

    1.2

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -1,10 +1,62 @@
+"use strict;"
+
 const heroEL = document.querySelector(".hero");
 const h1El = document.querySelector("h1");
+let xValue = 0;
+let yValue = 0;
 
-function changeShadow(e){
-    console.log(e)
-    const h1Rect = h1El.getBoundingClientRect();
-    console.log(h1Rect)
+function debounce(func, wait = 20, immediate = true) {
+    let timeout;
+    return function(...args) {
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+  
+      const later = () => {
+        timeout = null;
+        if (!immediate) {
+          func.apply(this, args);
+        }
+      };
+  
+      timeout = setTimeout(later, wait);
+  
+      if (callNow) {
+        func.apply(this, args);
+      }
+    }
 }
 
-heroEL.addEventListener('mousemove',changeShadow);
+function changeShadow(e){
+    console.log("X:",e.x,"Width-Mid:",window.innerWidth / 2,"Y:",e.y,"Height-mid",(window.innerHeight / 2 ));
+    if(e.x < (window.innerWidth / 2) && e.y < (window.innerHeight / 2)){
+        console.log("Top left","X:",xValue,"Y:",yValue);
+        if(xValue >=  -10)
+            {--xValue};
+        if(yValue >=  -10)
+            {--yValue};
+        h1El.style.textShadow = `${xValue}px ${yValue}px 5px rgba(0,0,0,1)`;
+    }else if(e.x > (window.innerWidth / 2) && e.y < (window.innerHeight / 2)){
+        console.log("top right",xValue,"Y:",yValue);
+        if(xValue <=  -10)
+            {++xValue};
+        if(yValue >=  -10)
+            {--yValue};
+        h1El.style.textShadow = `${xValue}px ${yValue}px 5px rgba(0,0,0,1)`;
+    }else if(e.x < (window.innerWidth / 2) && e.y > (window.innerHeight / 2)){
+        if(xValue <=  -10)
+            {--xValue};
+        if(yValue >=  -10)
+            {++yValue};
+        console.log("bottom left",xValue,"Y:",yValue);
+        h1El.style.textShadow = `${xValue}px ${yValue}px 5px rgba(0,0,0,1)`;
+    }else if(e.x > (window.innerWidth / 2) && e.y > (window.innerHeight / 2)){
+        if(xValue <=  -10)
+            {++xValue};
+        if(yValue >=  -10)
+            {++yValue};
+        console.log("bottom right",xValue,"Y:",yValue);
+        h1El.style.textShadow = `${xValue}px ${yValue}px 5px rgba(0,0,0,1)`;
+    }
+}
+
+heroEL.addEventListener('mousemove',(changeShadow));
